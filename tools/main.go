13a0aaf32b31a3f65063a726ca5ca08a2166fdfb
Luca Bianco <lbi@n-side.com>
2024-10-18 16:05:56 +0200

    sixth commit

--- tools/main.go
+++ tools/main.go
@@ -8,28 +8,3 @@ func GetVulnScore(d v1alpha1.Vulnerability) float64 {
 	}
 	return 0.0
 }
-
-
-.github	
-	workflows
-		pre-release.yml
-		release.yml
-charts
-	trivy-webhook-elasticsearch
-		templates
-			_helpers.templates
-			deployments.yaml
-			hpa.yaml
-			service.yaml
-			serviceaccount.yaml
-		Chart.yaml
-		readme.Methods
-		values.yaml
-tools
-	main.go
-.gitignore
-Dockerfile
-go.mod 
-go.sum 
-main.go
-read.me
